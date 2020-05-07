d5c778fccb46c673c8bee7407325aa396145ee65
tanveerhussain <tanveer_anjariya@hotmail.com>
2020-05-08 03:02:03 +0400

    Updates

---
+++ precache-manifest.480a6c1a707f29b73164e51b78f37414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19817cdbf0a3875d7b79ff14ef77e927",
+    "url": "/health-app/index.html"
+  },
+  {
+    "revision": "1f72e749a90541306043",
+    "url": "/health-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f16379ce8c7c618251d6",
+    "url": "/health-app/static/js/2.6c1b3bd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/health-app/static/js/2.6c1b3bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f72e749a90541306043",
+    "url": "/health-app/static/js/main.58be5bf7.chunk.js"
+  },
+  {
+    "revision": "d8c96cd109fa68f73005",
+    "url": "/health-app/static/js/runtime-main.891cab04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/health-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/health-app/precache-manifest.480a6c1a707f29b73164e51b78f37414.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/health-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
